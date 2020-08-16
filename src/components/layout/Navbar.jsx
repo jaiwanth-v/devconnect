91ff82bb929ec9be157620916d34edb44270d95d
Jaiwanth <jaiwanth2011@gmail.com>
2020-08-16 20:00:21 +0530

    Small change

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const toggleDarkMode = () => {
     setFetchMethod(window.fetch);
 
-    !isDark
+    !isDark && window.location.pathname !== "/"
       ? enableDarkMode({
           brightness: 100,
           contrast: 100,
